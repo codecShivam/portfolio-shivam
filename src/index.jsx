04cb0e0fbc7d58455eecfa6a40d64555863f259a
codecShivam <codecshivam@gmail.com>
2023-06-08 19:25:46 +0530

    code cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AppProvider } from '../src/components/Context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "../src/components/Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-			<App />
-		</AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Hero from "./About/About";
-// import Sidebar from "../components/Sidebar";
 
 const Homepage = () => {
-    return (
-        <div>
-            {/* <Sidebar /> */}
-            <Hero /> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
 
 export default Homepage;
