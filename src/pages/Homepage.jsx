3e88330e28ff0eb252aca5f3a29b251ccc75c0cc
Shivam Yadav <codecshivam@gmail.com>
2023-06-19 08:09:17 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import Hero from "./About/About";
-import Contact from "./Contact/Contact"
+import Hero from "./About/About";
+// import Contact from "./Contact/Contact"
 
 const Homepage = () => {
   
   return (
     <div>
-      {/* <Hero /> */}
-      <Contact /> 
+      <Hero /> 
+{/*       <Contact />  */}
     </div>
   );
 };
