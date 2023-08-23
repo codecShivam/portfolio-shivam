bdb6788301197703d23c852fc61ed8b3c9a957e0
codecShivam <codecshivam@gmail.com>
2023-08-23 14:30:02 +0530

    changed ui of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
-import "./nav.css";
-import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineUser } from "react-icons/ai";
-import { BiBook } from "react-icons/bi";
+import { useLocation } from "react-router-dom";
 import { RiServiceLine } from "react-icons/ri";
-import { BiMessageSquareDetail } from "react-icons/bi";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiMessageSquareDetail, BiBook } from "react-icons/bi";
+import { CSSTransition } from "react-transition-group";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import "./nav.css";
 
 const iconMap: Record<string, React.ReactElement> = {
   "": <AiOutlineHome />,
@@ -19,9 +18,10 @@ const iconMap: Record<string, React.ReactElement> = {
 const NavbarLink: React.FC<{
   to: string;
   icon: React.ReactElement;
+  name: string;
   active: boolean;
   onClick: () => void;
-}> = ({ to, icon, active, onClick }) => (
+}> = ({ to, icon, name, active, onClick }) => (
   <CSSTransition in={active} timeout={500} classNames="fade">
     <Link
       to={to}
@@ -30,25 +30,36 @@ const NavbarLink: React.FC<{
         active ? "active" : ""
       }`}
     >
-      {icon}
+      <div className={`icon-wrapper ${active ? "active" : ""}`}>
+        {icon}
+        {name === "" ? (
+          <span className="link-name">home</span>
+        ) : (
+          <span className="link-name">{name}</span>
+        )}
+      </div>
     </Link>
   </CSSTransition>
 );
 
 const Navbar: React.FC = () => {
-  const [activeNav, setActiveNav] = useState<string>("");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   return (
-    <nav className="nav">
-      {Object.keys(iconMap).map((navKey) => (
-        <NavbarLink
-          key={navKey}
-          to={`/${navKey}`}
-          icon={iconMap[navKey]}
-          active={activeNav === navKey}
-          onClick={() => setActiveNav(navKey)}
-        />
-      ))}
+    <nav>
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        {Object.keys(iconMap).map((navKey) => (
+          <NavbarLink
+            key={navKey}
+            to={`/${navKey}`}
+            icon={iconMap[navKey]}
+            name={navKey.charAt(0).toUpperCase() + navKey.slice(1)}
+            active={location.pathname === `/${navKey}`}
+            onClick={() => setMenuOpen(false)}
+          />
+        ))}
+      </div>
     </nav>
   );
 };
